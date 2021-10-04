1b79bb3268618987922ae3236f243552453bc106
willy24692485 <willy24692485@gmail.com>
2021-10-04 21:14:48 +0800

    sixth

--- module.py
+++ module.py
@@ -1,10 +1,10 @@
 # 載入內建的 sys(取得系統相關資訊) 模組並取得資訊
-import geometry
 import sys as system
 system.path.append("modules")  # 在模組的搜尋路徑列表中[新增路徑]
 print(system.platform)  # platform:作業系統
 print(system.maxsize)   # maxsize:整數型態最大值
 # 建立 geometry 模組，載入使用
+import geometry
 result = geometry.distance(1, 1, 4, 5)
 print(result)
 result = geometry.slope(1, 2, 5, 6)
